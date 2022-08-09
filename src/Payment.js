566760984d14134aa26f33506f2c9987858bc976
Odumbo <99364810+Odumbo@users.noreply.github.com>
2022-08-08 19:14:26 -0700

    Update Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -54,7 +54,7 @@ function Payment() {
         // paymentIntent = payment confirmation
 
         db.collection("users")
-          .doc(user?.uid)
+          .doc(user?.id)
           .collection("orders")
           .doc(paymentIntent.id)
           .set({
