370fe022c2ed28e9794512396309eae9d4335503
zdorf101 <99515272+zdorf101@users.noreply.github.com>
2022-08-09 17:18:01 -0700

    initiated backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"; //fetching library
 
 const instance = axios.create({
-  baseURL: "https://us-central1-challenge-4b2b2.cloudfunctions.net/api", //THE API (cloud function) URL
+  baseURL: "http://localhost:5001/challenge-4b2b2/us-central1/api", //THE API (cloud function) URL
 });
 
 export default instance;
